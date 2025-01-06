5029eebcfcf400f256b13bcaea46058870ff4385
amrosia <amrosia@tuta.io>
2025-01-06 14:40:55 +0500

    Implemented basic CLI argument handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use clap::Parser;
+
+const KRATER_ASCII: &str = r#"
+                           .      .
+        krater              )    (
+ _ _ _ _ _ _ _ _ _ _ _ _ _ _(.--.)
+( ) ) ) ) ) ) ) ) ) ) ) ) ) ( -_-) ==~
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>->>\/"#;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None, before_help = KRATER_ASCII)]
+struct Cli {
+    /// Email address to search
+    #[arg(long)]
+    email: Option<Vec<String>>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    // Handle emails if provided
+    if let Some(ref emails) = cli.email {
+        for email in emails {
+            println!("Will search for email: {}", email);
+        }
+    }
+
+    // Ensure at least one search parameter was provided
+    if cli.email.is_none() {
+        eprintln!("Error: At least one search parameter must be provided");
+        std::process::exit(1);
+    }
 }
